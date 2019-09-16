fc8fc38f7a90c3b9d19e748ce95aeb3cb777bc0b
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-16 16:27:29 +0200

    Update imports

--- frontend/src/containers/AllBooksOutput.js
+++ frontend/src/containers/AllBooksOutput.js
@@ -1,7 +1,8 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Query } from "react-apollo";
 import { Col, Row } from "react-bootstrap";
 import styled from "styled-components";
 import ItemCard from "../components/ItemCard";
+import { ALL_BOOKS_OUTPUT } from "../graphql/Queries";
 import "../styles/styles.css";
 
