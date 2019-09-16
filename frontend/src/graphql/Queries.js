3cfa1104d9b10eaf78bdaa0e9562b41dc296a84d
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-16 16:29:07 +0200

    Add -> AllBooksOutput Query

--- frontend/src/graphql/Queries.js
+++ frontend/src/graphql/Queries.js
@@ -5,3 +5,22 @@ export const HELLO_WORLD = gql `
  query helloWorld {
    hello
  }`;
+
+// GraphQL - All Books Output Query
+export const ALL_BOOKS_OUTPUT = gql `
+query AllBooksOutput{
+    allBooksOutput{
+      pk
+      sk
+      author
+      publisher
+      dateAdded
+      quantityOnHand
+      quantityOnOrder
+      quantityRequested
+      quantitySold
+      sellingPrice
+      costPrice
+    }
+  }
+`;
