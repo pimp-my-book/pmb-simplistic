11f6defd27c194522e687ffd2eaf95ae5256caac
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-16 14:19:17 +0200

    Update -> Query [allBooksOutput]

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -3,7 +3,8 @@ import { addBook } from "./mutation";
 
 export const resolvers = {
     Query: {
-        hello: (root, args, context) => hello(args, context)
+        hello: (root, args, context) => hello(args, context),
+        allBooksOutput: (root, args, context) => allBooksOutput(args, context)
     },
     Mutation: {
         addBook: (root, args, context) => addBook(args, context)
