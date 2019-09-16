da1318cbb95fe2ccc96caafaf15d9b908992c527
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-16 15:46:19 +0200

    Add -> BrowserRouter & Apollo imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-boost';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import './styles/styles.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
