4702168fb4abc76c0617554fb597cbccc8617a47
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-16 12:45:59 +0200

    Add -> devAddBook Mutation

--- backend/src/resolvers/mutation.js
+++ backend/src/resolvers/mutation.js
@@ -1,11 +1,39 @@
 import * as dynamodbLib from "../../resources/libs/dynamodb-lib";
 
-ADD YOUR MUTATION HERE:
-
-export const addCars = async (args, context) => {
-    
-
+export const devAddBook = async ({ input:args }, context) => {
+    const params = {
+        TableName: process.env.SimplisticTable,
+        Item: {
+            pk: args.pk,
+            sk: args.sk,
+            author: args.author,
+            publisher: args.publisher,
+            dateAdded: args.dateAdded,
+            quantityOnHand: args.quantityOnHand,
+            quantityOnOrder: args.quantityOnOrder,
+            quantityRequested: args.quantityRequested,
+            quantitySold: args.quantitySold,
+            sellingPrice: args.sellingPrice,
+            costPrice: args.costPrice
+        }
+    }
+    try {
+        await dynamodbLib.call("put", params);
+        return {
+            pk: args.pk,
+            sk: args.sk,
+            author: args.author,
+            publisher: args.publisher,
+            dateAdded: args.dateAdded,
+            quantityOnHand: args.quantityOnHand,
+            quantityOnOrder: args.quantityOnOrder,
+            quantityRequested: args.quantityRequested,
+            quantitySold: args.quantitySold,
+            sellingPrice: args.sellingPrice,
+            costPrice: args.costPrice
+        }
+    }
+    catch (e) {
+        console.log(e)
+    }
 }
-
-
-*/
