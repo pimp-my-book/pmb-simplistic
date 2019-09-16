6af08d202c0ade65aa6ba339ea0df05aa5525167
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-16 15:47:26 +0200

    export routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,3 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../src/containers/Home";
 
+export default () =>
+    <Switch>
+        <Route path="/" exact component={ Home } />
+    </Switch>
