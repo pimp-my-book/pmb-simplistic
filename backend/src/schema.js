d05ca8f6161106f1d456c8b9e01f9360de4c72f0
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-16 11:06:21 +0200

    Schema -> Input + defBookInput

--- backend/src/schema.js
+++ backend/src/schema.js
@@ -1,6 +1,19 @@
 const schema = `
 
-
+"""
+INPUT
+"""
+input defBookInput {
+    Author: String!,
+    Publisher: String!,
+    dateAdded: String!,
+    quantityOnHand: Int!,
+    quantityOnOrder: Int!,
+    quantityRequested: Int!,
+    quantitySold: Int!,
+    sellingPrice: Int!,
+    costPrice: Int!,
+}
 
 """
 A hello world Query
