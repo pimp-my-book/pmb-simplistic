cadc1d8d4e838ce1c8c07223d4a7284c47f6b2cf
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-16 16:10:06 +0200

    Add -> Heading typography

---
+++ frontend/src/components/typography/Heading.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+ const Heading = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+  font-family: 'Poppins', sans-serif;
+  color:#1b335f;
+`;
+
+export default Heading;
