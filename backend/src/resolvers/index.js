e28cfbffed1020eaaf86132a0873c2802b56836a
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-16 12:45:29 +0200

    Add -> devAddBook mutation

--- backend/src/resolvers/index.js
+++ backend/src/resolvers/index.js
@@ -4,5 +4,8 @@ import { devAddBook } from "./mutation";
 export const resolvers = {
     Query: {
         hello: (root, args, context) => hello(args, context)
+    },
+    Mutation: {
+        devAddBook: (root, {input:args}, context) => devAddBook({input:args}, context)
     }
 }
