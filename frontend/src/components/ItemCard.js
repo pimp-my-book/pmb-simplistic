cf55ac2b9903010186dba4c5694f7f80f7c666db
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-16 16:09:33 +0200

    Add -> custom styles to card

--- frontend/src/components/ItemCard.js
+++ frontend/src/components/ItemCard.js
@@ -3,3 +3,14 @@ import styled from "styled-components";
 import Heading from "./typography/Heading";
 import { Card, Container, Row, Col } from "react-bootstrap";
 
+const CardStyles = styled(Card) `
+  &&& {
+    width: 300px;
+    height: 150px;
+    padding: 5%;
+    box-shadow: 0px 2px 4px rgba(0,0,0,0.18);
+    background-color: white;
+    }
+  }
+`;
+
