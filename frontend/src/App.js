b4283c753527f9e801fd86c8fb18d59f400c0f80
krampus-nuggets <sp3cter.productions@wemakeart.co.za>
2019-09-16 15:45:25 +0200

    Import routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
+import Routes from "./Routes";
 import "./styles/styles.css";
 
 class App extends Component {
